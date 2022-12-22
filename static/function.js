8476ec4e4daf93f98ca20e03eeffea2284560f40
Glitch (reidinc) <none>
2022-12-22 17:04:09 +0000

    🚆💂 Checkpoint ./static/function.js:52075789/402 ./static/index.html:52075789/211621 ./.glitch-assets:52075789/363

---
+++ static/function.js
@@ -0,0 +1,118 @@
+
+var correctuser = "bob"
+var correctpass = "sus"
+var correct = false
+while ( correct === false){
+  let user = prompt("Username");
+  let pass = prompt("Password");
+  if (user === "bob" && pass==="sus"){
+    correct = true;
+    break;
+  } else {
+    alert("THERE IS AN IMPOSTER AMONG US!!!!!")
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
