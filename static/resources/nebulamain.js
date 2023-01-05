fbe84c950cc5fa8e83aa4a5bd18308fb33702209
Glitch (reidinc) <none>
2023-01-05 20:57:24 +0000

    🚧💃 Checkpoint ./static/index.html:52075789/1455 ./static/style/main.css:52075789/104 ./static/resources/nebulamain.js:52075789/621

--- static/resources/nebulamain.js
+++ static/resources/nebulamain.js
@@ -42,30 +42,7 @@ scope: '/service/'
   var td = new Date();
 
   function showTime() {
-    var date = new Date();
-    var h = date.getHours(); // 0 - 23
-    var m = date.getMinutes(); // 0 - 59
-    var s = date.getSeconds(); // 0 - 59
-
-    var session = "AM";
-
-    if (h == 0) {
-      h = 12;
-    }
-
-    if (h > 12) {
-      h = h - 12;
-      session = "PM";
-    }
-
-    h = (h < 10) ? "0" + h : h;
-    m = (m < 10) ? "0" + m : m;
-    s = (s < 10) ? "0" + s : s;
-    setTimeout(showTime, 1000);
-    var time = h + "<span style='opacity:100%;' class='clockColon'>:</span>" + m;
-    document.getElementById('digitalClock').innerHTML = getDayName(today, "us-US") + ", " + time + " " + session + ".";
-
-    return time;
+    
   }
 
   showTime();
